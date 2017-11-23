be7d42cc8e10125680ddb5d0a92138049ff3a7a8
张旭 <zhangxu@ihealthlabs.com.cn>
2017-11-23 17:53:35 +0800

    1、补充

---
+++ src/app/services/pagination.service.ts
@@ -0,0 +1,42 @@
+/**
+ * Created by zhangxu on 2017/11/23.
+ */
+import {Injectable} from '@angular/core';
+import {LicenseService} from './license.service';
+import {License} from '../model/License';
+
+@Injectable()
+export class PaginationService {
+
+
+  pageSize = 2;// 每页数据条数
+  curPage = 1;// 当前页码
+  constructor(private licenseService: LicenseService) {}
+
+  paginationChange(page: number, licenses: License[]): any {
+    console.log('page = ' + page);
+    console.log('licenses = ' + licenses);
+
+    let licensesAll;
+    if (licenses === null) {
+      licensesAll = this.licenseService.licenses;
+    } else {
+      licensesAll = licenses;
+    }
+
+    console.log('licenseAll = ' + JSON.stringify(licensesAll));
+    if (page === 0) {
+
+      const start = this.pageSize * (this.curPage - 1);
+      const end = this.pageSize * this.curPage;
+      return licensesAll.slice(start, end);
+    } else {
+
+      this.curPage = page;
+      const start = this.pageSize * (page - 1);
+      const end = this.pageSize * page;
+      return licensesAll.slice(start, end);
+    }
+
+  }
+}
