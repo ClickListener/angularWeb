417d3cd9fc484b0b8749c107d7e93f65ead9a1a2
Jeepend <jeepend@foxmail.com>
2017-12-08 11:34:24 +0800

    Add materialize.

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -7,6 +7,8 @@ import {User} from "../../model/User";
 import {UserService} from "../../services/user.service";
 import {Router} from "@angular/router";
 import {isUndefined} from "util";
+declare const $: any;
+declare const M: any;
 @Component({
     selector: 'my-app',
 
@@ -18,11 +20,15 @@ export class AppComponent implements OnInit, OnChanges, DoCheck {
 
 
     user: User;
+    private sideNav: any;
 
     constructor(private userService:UserService, private router:Router) {}
 
     ngOnInit(): void {
         console.log('ngOnInit()');
+      const elem = document.querySelector('.sidenav');
+      this.sideNav = new M.Sidenav(elem);
+
     }
 
     ngOnChanges(changes: SimpleChanges): void {
@@ -40,9 +46,19 @@ export class AppComponent implements OnInit, OnChanges, DoCheck {
             })
     }
 
-    ngDoCheck(): void {
-        console.log('ngDoCheck()');
+    closeNav(): void {
+      this.sideNav.close();
+      $('#container').css({"padding-left": "0px"});
+    }
 
+    openNav(): void {
+      this.sideNav.open();
+      if ($(window).width() > 922) {
+        $('#container').css({"padding-left": "300px"});
+      }
+    }
+
+    ngDoCheck(): void {
         this.user = this.userService.user;
     }
 
