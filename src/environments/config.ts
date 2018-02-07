e2eccbdfc8d9cd9e9cc8ba4933a9250018971c67
张旭 <zhangxu@ihealthlabs.com.cn>
2018-02-07 11:13:47 +0800

    test server

--- src/environments/config.ts
+++ src/environments/config.ts
@@ -1,5 +1,5 @@
 /**
  * Created by zhangxu on 2018/2/7.
  */
-// export const url = "http://192.168.69.111:3001";
-export const url = "http://localhost:3001";
+export const url = "http://192.168.69.111:3001";
+// export const url = "http://localhost:3001";
