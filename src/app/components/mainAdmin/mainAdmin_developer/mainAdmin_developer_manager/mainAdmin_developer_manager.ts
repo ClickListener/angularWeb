4fd3f9f3eade1dce1d1e9788884af98ee68ce160
张旭 <zhangxu@ihealthlabs.com.cn>
2018-03-01 10:27:56 +0800

    1.Add setting of main developer check beta.

--- src/app/components/mainAdmin/mainAdmin_developer/mainAdmin_developer_manager/mainAdmin_developer_manager.ts
+++ src/app/components/mainAdmin/mainAdmin_developer/mainAdmin_developer_manager/mainAdmin_developer_manager.ts
@@ -3,6 +3,7 @@
  */
 import {Component} from "@angular/core";
 import {UserService} from "../../../../services/user.service";
+import swal from "sweetalert2";
 
 @Component({
   templateUrl: './mainAdmin_developer_manager.html',
@@ -28,6 +29,36 @@ export class MainAdminDeveloperManagerComponent {
 
         if (res.success) {
           this.developerList = res.data;
+
+          this.developerList.find((developer, index, arr) => {
+            if (developer._id === userService.user._id) {
+              arr.splice(index, 1);
+              this.developerList = arr;
+            }
+            return developer._id === userService.user._id;
+          });
+
+
+          this.developerList.forEach((developer) => {
+
+            if (developer.type === 3) {
+              const user_Info = {
+                "userId": userService.user._id,
+                "token": userService.token.token,
+                "uid": developer._id
+              };
+
+              userService.getUserAuth(user_Info)
+                .then(response => {
+                  console.log('res = ', response);
+                  this.parsePermission(response.data, developer);
+                })
+                .catch(error => {
+                  console.log('error = ', error);
+                });
+            }
+
+          });
         }
 
       })
@@ -36,5 +67,67 @@ export class MainAdminDeveloperManagerComponent {
       });
 
 
+
+
+  }
+
+  private parsePermission(permission: any, developer: any) {
+
+    console.log('permission = ', permission);
+
+    permission.forEach((item, index) => {
+      if (item.resourceId === '5a6580ca5e149e1dfdf27962') {
+        developer.beta = (item.action.indexOf('R') !== -1);
+        console.log('beta = ', developer.beta);
+        return;
+      }
+
+    });
+  }
+
+
+  updateBetaPermission(developer: any) {
+
+    console.log('developer=', developer);
+
+    const permissionArr = [];
+
+    const betaPermission = {
+      "rid": '5a6580ca5e149e1dfdf27962',
+      "action": []
+    };
+
+    if (developer.beta === 'true') {
+      betaPermission.action.push('R');
+    }
+
+    permissionArr.push(betaPermission);
+
+
+    const permissionInfo = {
+      "userId": this.userService.user._id,
+      "token": this.userService.token.token,
+      "uid": developer._id,
+      "companyId": developer.companyId,
+      "auth": permissionArr,
+      "isValid": true
+    };
+
+    this.userService.addUserAuth(permissionInfo)
+      .then(res => {
+        console.log(res);
+        swal({
+          position: 'center',
+          type: 'success',
+          titleText: 'Update success',
+          showConfirmButton: false,
+          timer: 1500,
+          padding: 0,
+          width: 300
+        }).catch(swal.noop);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 }
--- src/app/components/mainAdmin/mainAdmin_developer/mainAdmin_developer_modify/mainAdmin_developer_modify.ts
+++ src/app/components/mainAdmin/mainAdmin_developer/mainAdmin_developer_modify/mainAdmin_developer_modify.ts
@@ -2,7 +2,8 @@
  * Created by zhangxu on 2018/2/8.
  */
 import {Component} from "@angular/core";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {UserService} from "../../../../services/user.service";
 
 @Component({
   templateUrl: './mainAdmin_developer_modify.html',
@@ -12,11 +13,86 @@ import {ActivatedRoute} from "@angular/router";
 
 export class MainAdminDeveloperModifyComponent {
 
-  param: string;
-  constructor(private activatedRoute: ActivatedRoute) {
+  id: string;
+
+  companyId: string;
+
+  beta = false;
+
+  constructor(private activatedRoute: ActivatedRoute, private userService: UserService, private router: Router) {
 
     activatedRoute.paramMap.subscribe(paramMap => {
-      this.param = paramMap['params'].param;
+
+      this.id = paramMap['params'].id;
+      this.companyId = paramMap['params'].companyId;
     });
+
+    const user_Info = {
+      "userId": userService.user._id,
+      "token": userService.token.token,
+      "uid": this.id
+    };
+
+   userService.getUserAuth(user_Info)
+     .then(res => {
+        console.log('res = ', res);
+        this.parsePermission(res.data);
+     })
+     .catch(error => {
+       console.log('error = ', error);
+     });
+
+
+  }
+
+
+  private parsePermission(permission: any) {
+
+    console.log('permission = ', permission);
+
+    permission.forEach((item, index) => {
+      if (item.resourceId === '5a6580ca5e149e1dfdf27962') {
+        this.beta = (item.action.indexOf('R') !== -1);
+        console.log('beta = ', this.beta);
+        return;
+      }
+
+    });
+  }
+
+
+  updateBetaPermission() {
+
+    const permissionArr = [];
+
+    const betaPermission = {
+      "rid": '5a6580ca5e149e1dfdf27962',
+      "action": []
+    };
+
+    if (this.beta) {
+      betaPermission.action.push('R');
+    }
+
+    permissionArr.push(betaPermission);
+
+
+    const permissionInfo = {
+      "userId": this.userService.user._id,
+      "token": this.userService.token.token,
+      "uid": this.id,
+      "companyId": this.companyId,
+      "auth": permissionArr,
+      "isValid": true
+    };
+
+    this.userService.addUserAuth(permissionInfo)
+      .then(res => {
+        console.log(res);
+        this.router.navigate(['/developer-manager']);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 }
