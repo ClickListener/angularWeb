f3f3220b3d4ba5c222c16220e3cdf66b3a69963d
张旭 <zhangxu@ihealthlabs.com.cn>
2018-04-08 17:22:28 +0800

    1.Add modify view

--- src/app/components/mainAdmin/mainAdmin_developer/mainAdmin_secondMaster_modify/mainAdmin_secondMaster_modify.ts
+++ src/app/components/mainAdmin/mainAdmin_developer/mainAdmin_secondMaster_modify/mainAdmin_secondMaster_modify.ts
@@ -2,7 +2,9 @@
  * Created by zhangxu on 2018/4/3.
  */
 import {Component} from "@angular/core";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {UserService} from "../../../../services/user.service";
+import swal from "sweetalert2";
 
 @Component({
   templateUrl: './mainAdmin_secondMaster_modify.html',
@@ -13,11 +15,72 @@ export class MainAdminSecondMasterModifyComponent {
 
 
   id: string;
+  admin: any;
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(private activatedRoute: ActivatedRoute, private userService: UserService, private router: Router) {
     activatedRoute.paramMap.subscribe((paramMap) => {
       this.id = paramMap['params'].param;
+
+      const userInfo = {
+        "userId": userService.user._id,
+        "token": userService.token.token,
+        "uid": this.id
+      };
+
+      this.userService.getUserInfo(userInfo)
+        .then(res => {
+          console.log(res);
+          if (res.success) {
+            this.admin = res.user;
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        });
+
     });
+  }
+
+  updateSecondMaster() {
+
+    const updateInfo = {
+      'userId': this.userService.user._id,
+      'token': this.userService.token.token,
+      'user': this.admin
+    };
+
+    this.userService.updateUser(updateInfo)
+      .then(res => {
+
+        console.log(res);
+
+        if (res.success) {
+          this.router.navigate(['/secondMaster/secondMaster-manager']);
+          swal({
+            position: 'center',
+            type: 'success',
+            titleText: 'Create success',
+            showConfirmButton: false,
+            timer: 2000,
+            padding: 0,
+            width: 300
+          }).catch(swal.noop);
+        } else {
+          swal({
+            position: 'center',
+            type: 'error',
+            titleText: res.message,
+            showConfirmButton: false,
+            timer: 2000,
+            padding: 0,
+            width: 300
+          }).catch(swal.noop);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
   }
 
 
