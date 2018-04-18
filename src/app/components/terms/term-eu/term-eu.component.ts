043d3bd908257c0ceca87b2a7b799462fd5ca357
张旭 <zhangxu@ihealthlabs.com.cn>
2018-04-18 17:20:32 +0800

    1.Fix bug

--- src/app/components/terms/term-eu/term-eu.component.ts
+++ src/app/components/terms/term-eu/term-eu.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {UserService} from "../../../services/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-term-eu',
   templateUrl: './term-eu.component.html',
   styleUrls: ['./term-eu.component.css'],
-  encapsulation: ViewEncapsulation.None
+  // encapsulation: ViewEncapsulation.None
 })
 export class TermEuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) {
+    if (userService.user) {
+      router.navigate(['/']);
+    }
+  }
 
   ngOnInit() {
   }
