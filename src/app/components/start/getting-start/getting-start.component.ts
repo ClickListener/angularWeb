af52c4ab24b95f80c3b1a6cea9d712c3abf7f194
张旭 <zhangxu@ihealthlabs.com.cn>
2018-04-27 16:49:08 +0800

    1.getting-start view

---
+++ src/app/components/start/getting-start/getting-start.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-getting-start',
+  templateUrl: './getting-start.component.html',
+  styleUrls: ['./getting-start.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class GettingStartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
